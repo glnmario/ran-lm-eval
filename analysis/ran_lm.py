b33e2899bcde9c28c5bc67f5f2bfabcf3bd36949
Mario <m.giulianelli.m@gmail.com>
2017-12-12 19:28:42 +0100

    Run analysis for list of sentences

--- analysis/ran_lm.py
+++ analysis/ran_lm.py
@@ -39,93 +39,101 @@ with open(model_path, 'rb') as f:
 w2i = corpus.dictionary.word2idx
 ntokens = len(corpus.dictionary)
 
-# convert sentence to a list of word indices
-input_word_indices = [w2i[w] for w in sent_aslist]
-input_word_indices = Variable(torch.LongTensor(input_word_indices))
+with open('text.txt', 'r') as f_in, open('out.txt', 'w') as f_out:
+    for line in f_in:
+        sent = line.strip().split()
 
-# delete old output if necessary
-try:
-    os.remove('./values.txt')
-except FileNotFoundError:
-    pass
+        verbs = []
+        idx = None
 
+        for i, word in enumerate(sent):
+            if "/" in word:
+                verbs = word[1:].split("/")
+                stop_idx = i
 
-# initialise hidden for first timestep
-hidden = model.init_hidden(batch_size=1)
-# forward pass
-output, hidden = model(input_word_indices, hidden)
-# linear output
-output_flat = output.view(-1, ntokens)
-# apply softmax manually
-softmax = nn.Softmax()
-output_flat = softmax(output_flat)
+        sent_len = stop_idx + 1
 
+        sentences = []
+        sentences_with_ids = []
+        for verb in verbs:
+            # convert sentence to a list of word indices
+            input_word_indices = [w2i[w] for i, w in enumerate(sent) if i < stop_idx] + [w2i[verb]]
+            sentences_with_ids.append(Variable(torch.LongTensor(input_word_indices)))
+            sentences.append([w for i, w in enumerate(sent) if i < stop_idx] + [verb])
 
-############################################################
-########################  ANALYSIS  ########################
+        # delete old output if necessary
+        try:
+            os.remove('./values.txt')
+        except FileNotFoundError:
+            pass
 
-# compute sentence probability and perplexity
-p = 1
-for i, w in enumerate(sent_aslist):
-    p *= output_flat[i][w2i[w]].data[0]
-
-pp = p ** (-1/sentence_len)
-
-print('\n>> {} <<'.format(sentence))
-print('Probability:', p)
-print('Perplexity:', pp, '\n')
-
-
-# now we recover values for candidates, input gates, and
-# forget gates, which have been saved during forward pass
-ctilde_list = []
-i_list = []
-f_list = []
-
-with open('./values.npy', 'rb') as f:
-    values = np.load(f)
-
-    ctilde_list = values[0]
-    i_list = values[1]
-    f_list = values[2]
-
-# compute 3D matrix fo weights
-w = np.zeros((sentence_len, sentence_len, ctilde_list.shape[0]))
-for t in range(sentence_len-1):
-    for j in range(sentence_len-1):
-        f_prod = 1
-        for k in range(j+1, t):
-            f_prod *= f_list[k]
-
-        w[t][j] = i_list[j] * f_prod
-
-print("weights ", w.shape)
-
-# for each word, print the most active history word and the list of all activations
-w_c_all = []
-for t, word in enumerate(sent_aslist[:-1]):
-
-    if t == 0:
-        print(word, '[]\n', sep='\n')
-        continue
-
-    sums = np.zeros((t, ctilde_list.shape[0]))
-
-    for i in range(t):
-        sums[i] = (w[t][i] * ctilde_list[i])
-
-    w_c_all.append(sums)
+        for idx, s_with_ids in enumerate(sentences_with_ids):
+            # initialise hidden for first timestep
+            hidden = model.init_hidden(batch_size=1)
+            # forward pass
+            output, hidden = model(s_with_ids, hidden)
+            # linear output
+            output_flat = output.view(-1, ntokens)
+            # apply softmax manually
+            softmax = nn.Softmax()
+            output_flat = softmax(output_flat)
 
 
 
+            ############################################################
+            ########################  ANALYSIS  ########################
 
-for l,k in enumerate(w_c_all):
-    words = sentence.split()[1:-1]
-    activations = np.sum(k, axis=1)
+            # compute sentence probability and perplexity
+            p = 1
+            for i, w in enumerate(s_with_ids):
+                p *= output_flat[i][w].data[0]
 
-    print(activations.shape)
-    print(words[l], '->', sent_aslist[np.argmax(activations)])
-    print(activations, '\n')
+            pp = p ** (-1 / sent_len)
 
-print(ctilde_list.shape[0])
+            print('\n>> {} <<'.format(sentences[idx]), file=f_out)
+            print('Probability:', p, file=f_out)
+            print('Perplexity:', pp, '\n', file=f_out)
 
+
+            # now we recover values for candidates, input gates, and
+            # forget gates, which have been saved during forward pass
+            ctilde_list = []
+            i_list = []
+            f_list = []
+
+            with open('./values.npy', 'rb') as f:
+                values = np.load(f)
+
+                ctilde_list = values[0]
+                i_list = values[1]
+                f_list = values[2]
+
+            # compute 3D matrix fo weights
+            w = np.zeros((sent_len, sent_len, ctilde_list.shape[0]))
+            for t in range(sent_len):
+                for j in range(sent_len):
+                    f_prod = 1
+                    for k in range(j+1, t):
+                        f_prod *= f_list[k]
+
+                    w[t][j] = i_list[j] * f_prod
+
+            # for each word, print the most active history word and the list of all activations
+            w_c_all = []
+            for t in range(sent_len):
+                if t == 0:
+                    print(sentences[idx][t], '[]\n', sep='\n', file=f_out)
+                    continue
+
+                sums = np.zeros((sent_len, ctilde_list.shape[0]))
+                for i in range(t):
+                    sums[i] = w[t][i] * ctilde_list[i]
+
+                w_c_all.append(sums)
+
+            for l, k in enumerate(w_c_all):
+                activations = np.sum(k, axis=1)
+
+                print(sentences[idx][l+1], '->', sentences[idx][np.argmax(activations)], file=f_out)
+                print(activations, '\n', file=f_out)
+        print('\n\n', file=f_out)
