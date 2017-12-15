correct computation of w

analysis/ran_lm.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 import data
 import model
@@ -19,13 +21,19 @@ def pretty_print(sentence, activations, t, f_out=None):
     relative_order = [influencers_sorted.index(x) for x in list(activations[:t+1])]
 
     num_str = '  '
-    for i, word in enumerate(sentence[:t+1]):
+    for i, word in enumerate(sentence[:t]):
         num_str += str(relative_order[i]) + ' ' * (len(word)+3)
 
-    print(sentence[t+1], '->', sentence[np.argmax(activations)], file=f_out)
+    print(sentence[t], '->', sentence[np.argmax(activations)], file=f_out)
     print(num_str, file=f_out)
-    print(sentence[:t+2], file=f_out)
-    print(activations[:t+1], '\n', file=f_out)
+    print(sentence[:t+1], file=f_out)
+    print(activations[:t], '\n', file=f_out)
+
+
+def plot_influence_sent(inf_matrix, sent):
+    assert len(inf_matrix)==len(sent)
+    plt.matshow(inf_matrix, cmap=plt.cm.Blues)
+    plt.show()
 
 
 #-------------------------------------------------#
@@ -44,8 +52,9 @@ w2i = corpus.dictionary.word2idx
 ntokens = len(corpus.dictionary)
 
 # Method for computing the most influential word
-# most_influential_word_mode = 'max_w'
-most_influential_word_mode = 'l1_c'
+most_influential_word_mode = 'max_w'
+# most_influential_word_mode = 'l2'
+# most_influential_word_mode = 'l1_c'
 w_old = None
 c_old = None
 
@@ -135,22 +144,14 @@ with open('sentences/{}'.format(filename), 'r') as f_in, open('sentences/'+'out_
             # compute 3D matrix fo weights
             w = np.zeros((sent_len, sent_len, ctilde_list.shape[1]))
             for t in range(sent_len):
-                for j in range(t):
+                for j in range(t+1):
                     f_prod = np.ones((f_list.shape[1]))
-                    for k in range(j+1, t):
+                    for k in range(j+1, t+1):
                         f_prod *= f_list[k]
 
                     w[t][j] = i_list[j] * f_prod
 
-            if w_old is not None:
-                print(np.any(w!=w_old))
-            else:
-                w_old = np.copy(w)
 
-            if c_old is not None:
-                print(np.any(ctilde_list!=c_old))
-            else:
-                c_old = np.copy(ctilde_list)
              # for each word, print the most active history word and the list of all activations
 
             if most_influential_word_mode == 'l1_c':
@@ -177,24 +178,23 @@ with open('sentences/{}'.format(filename), 'r') as f_in, open('sentences/'+'out_
 
 
             elif most_influential_word_mode == 'max_w':
-                w_all = []
-                for t in range(sent_len):
-                    if t == 0:
-                        print(sentences[idx][t], '[]\n', sep='\n', file=f_out)
-                        continue
-
-                    w_vec = np.zeros((sent_len, f_list.shape[1]))
-                    for i in range(t):
-                        w_vec[i] = w[t][i]
-
-                    w_all.append(w_vec)
-
-
-                for t, w_vec in enumerate(w_all):
+                influence_matrix = np.zeros((sent_len, sent_len))
+                for t, w_vec in enumerate(w):
                     activations = np.max(np.absolute(w_vec), axis=1)
+                    influence_matrix[t] = activations
 
                     pretty_print(sentences[idx], activations, t, f_out)
-                    #print(sentences[idx][t+1], '->', sentences[idx][np.argmax(activations)], file=f_out)
-                    #print(activations[:t+1], '\n', file=f_out)
+
+                #plot_influence_sent(influence_matrix.T, sentences[idx])
+
+            elif most_influential_word_mode == 'l2':
+                influence_matrix = np.zeros((sent_len, sent_len))
+                for t, w_vec in enumerate(w):
+                    activations = np.linalg.norm(w_vec, axis=1)
+                    influence_matrix[t] = activations
+
+                    pretty_print(sentences[idx], activations, t, f_out)
+
+                #plot_influence_sent(influence_matrix.T, sentences[idx])
         print('\n\n', file=f_out)
         break