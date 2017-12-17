c076535a6a4b7ef7184c22eebdbc9fc1253322b1
Procope <m.giulianelli.m@gmail.com>
2017-12-17 22:03:23 +0100

    Add regex for adjacent verb form government

--- analysis/regex.py
+++ analysis/regex.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 30 10:59:47 2017
-
 @author: mario
 """
 
@@ -30,30 +28,32 @@ with open('../data/penn/test.txt', 'r') as f_in:
         corpus.append(line.strip())
 
 
+regex_ed_0 = r'\bha(ve|s|d) \w+ed\b'
 regex_ed_1 = r'\bha(ve|s|d) ((\w|\d)+ ){1}\w+ed\b'
 regex_ed_2 = r'\bha(ve|s|d) ((\w|\d)+ ){2}\w+ed\b'
 regex_ed_3 = r'\bha(ve|s|d) ((\w|\d)+ ){3}\w+ed\b'
 regex_ed_4 = r'\bha(ve|s|d) ((\w|\d)+ ){4}\w+ed\b'
 regex_ed_5 = r'\bha(ve|s|d) ((\w|\d)+ ){5}\w+ed\b'
 
+regex_ing_0 = r'\b(are|am|is) \w+(ed|ing)\b'
 regex_ing_1 = r'\b(are|am|is) ((\w|\d)+ ){1}\w+(ed|ing)\b'
 regex_ing_2 = r'\b(are|am|is) ((\w|\d)+ ){2}\w+(ed|ing)\b'
 regex_ing_3 = r'\b(are|am|is) ((\w|\d)+ ){3}\w+(ed|ing)\b'
 regex_ing_4 = r'\b(are|am|is) ((\w|\d)+ ){4}\w+(ed|ing)\b'
 regex_ing_5 = r'\b(are|am|is) ((\w|\d)+ ){5}\w+(ed|ing)\b'
 
-regex_irreg_1 = r'\bha(ve|s|d) ((\w|\d)+ ){1}(read|made|gone|done|come|got|bought|sought|been|seen|thought)\b'
-regex_irreg_2 = r'\bha(ve|s|d) ((\w|\d)+ ){2}(read|made|gone|done|come|got|bought|sought|been|seen|thought)\b'
-regex_irreg_3 = r'\bha(ve|s|d) ((\w|\d)+ ){3}(read|made|gone|done|come|got|bought|sought|been|seen|thought)\b'
-regex_irreg_4 = r'\bha(ve|s|d) ((\w|\d)+ ){4}(read|made|gone|done|come|got|bought|sought|been|seen|thought)\b'
-regex_irreg_5 = r'\bha(ve|s|d) ((\w|\d)+ ){5}(read|made|gone|done|come|got|bought|sought|been|seen|thought)\b'
+regex_irreg_0 = r'\bha(ve|s|d) (read|made|gone|done|come|got|bought|sought|seen|thought)\b'
+regex_irreg_1 = r'\bha(ve|s|d) ((\w|\d)+ ){1}(read|made|gone|done|come|got|bought|sought|seen|thought)\b'
+regex_irreg_2 = r'\bha(ve|s|d) ((\w|\d)+ ){2}(read|made|gone|done|come|got|bought|sought|seen|thought)\b'
+regex_irreg_3 = r'\bha(ve|s|d) ((\w|\d)+ ){3}(read|made|gone|done|come|got|bought|sought|seen|thought)\b'
+regex_irreg_4 = r'\bha(ve|s|d) ((\w|\d)+ ){4}(read|made|gone|done|come|got|bought|sought|seen|thought)\b'
+regex_irreg_5 = r'\bha(ve|s|d) ((\w|\d)+ ){5}(read|made|gone|done|come|got|bought|sought|seen|thought)\b'
 
 
-regs = [regex_ed_1, regex_ed_2, regex_ed_3, regex_ed_4, regex_ed_5,
-        regex_ing_1, regex_ing_2, regex_ing_3, regex_ing_4, regex_ing_5,
-        regex_irreg_1, regex_irreg_2, regex_irreg_3, regex_irreg_4,
+regs = [regex_ed_0, regex_ed_1, regex_ed_2, regex_ed_3, regex_ed_4, regex_ed_5,
+        regex_ing_0, egex_ing_1, regex_ing_2, regex_ing_3, regex_ing_4, regex_ing_5,
+        regex_irreg_0, regex_irreg_1, regex_irreg_2, regex_irreg_3, regex_irreg_4,
         regex_irreg_5]
-
 
 for reg in regs:
     filename = namestr(reg, globals())[0]
